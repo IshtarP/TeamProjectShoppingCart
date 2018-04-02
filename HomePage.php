e03ca7436d5e11399987765b9f115aaa7fd89a7d
Abraham De Alba <adealba@csumb.edu>
2018-04-02 23:44:22 +0000

    homepage

---
+++ HomePage.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body class="main">
+        <div class="homecontainer">
+            <h1>Welcome to BetterGamestahp</h1>
+            <h3>Home</h3>
+            <hr width="60%"/>
+            <div class="formDiv">
+                <form action="ConsolePage.php">
+                    <input type="submit" value="Console">
+                </form>
+                
+                <form action="PCPage.php">
+                    <input type="submit" value="PC">
+                </form>
+                
+                <form action="MobilePage.php">
+                    <input type="submit" value="Mobile">
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
